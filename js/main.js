9204f488d73621c5de0823a7a4cd8f0f6e6bf1f9
portfolio-miguelrodfher <miguelrodfhercareer@gmail.com>
2023-02-06 12:39:04 -0500

    added a vew counter

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
-;(function () {
-	
+; (function () {
+
 	'use strict';
 
 
 
 	// iPad and iPod detection	
-	var isiPad = function(){
+	var isiPad = function () {
 		return (navigator.platform.indexOf("iPad") != -1);
 	};
 
-	var isiPhone = function(){
-	    return (
-			(navigator.platform.indexOf("iPhone") != -1) || 
+	var isiPhone = function () {
+		return (
+			(navigator.platform.indexOf("iPhone") != -1) ||
 			(navigator.platform.indexOf("iPod") != -1)
-	    );
+		);
 	};
 
 
-	var fullHeight = function() {
+	var fullHeight = function () {
 
 		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
+		$(window).resize(function () {
 			$('.js-fullheight').css('height', $(window).height());
 		});
 
 	};
 
-	var burgerMenu = function() {
+	var burgerMenu = function () {
 
-		$('.js-my_resume-nav-toggle').on('click', function(event) {
+		$('.js-my_resume-nav-toggle').on('click', function (event) {
 			event.preventDefault();
 			var $this = $(this);
-			if( $('body').hasClass('menu-show') ) {
+			if ($('body').hasClass('menu-show')) {
 				$('body').removeClass('menu-show');
 				$('#my_resume-main-nav > .js-my_resume-nav-toggle').removeClass('show');
 			} else {
 				$('body').addClass('menu-show');
-				setTimeout(function(){
+				setTimeout(function () {
 					$('#my_resume-main-nav > .js-my_resume-nav-toggle').addClass('show');
 				}, 900);
 			}
@@ -45,147 +45,148 @@
 
 	// Animations
 
-	var contentWayPoint = function() {
+	var contentWayPoint = function () {
 		var i = 0;
-		$('.animate-box').waypoint( function( direction ) {
+		$('.animate-box').waypoint(function (direction) {
 
-			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-				
+			if (direction === 'down' && !$(this.element).hasClass('animated')) {
+
 				i++;
 
 				$(this.element).addClass('item-animate');
-				setTimeout(function(){
+				setTimeout(function () {
 
-					$('body .animate-box.item-animate').each(function(k){
+					$('body .animate-box.item-animate').each(function (k) {
 						var el = $(this);
-						setTimeout( function () {
+						setTimeout(function () {
 							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
+							if (effect === 'fadeIn') {
 								el.addClass('fadeIn animated');
 							} else {
 								el.addClass('fadeInUp animated');
 							}
 
 							el.removeClass('item-animate');
-						},  k * 200, 'easeInOutExpo' );
+						}, k * 200, 'easeInOutExpo');
 					});
-					
+
 				}, 100);
-				
+
 			}
 
-		} , { offset: '85%' } );
+		}, { offset: '85%' });
 	};
 
 
-	var counter = function() {
+	var counter = function () {
 		$('.js-counter').countTo({
-			 formatter: function (value, options) {
-	      return value.toFixed(options.decimals);
-	    },
+			formatter: function (value, options) {
+				return value.toFixed(options.decimals);
+			},
 		});
 	};
 
-	var counterWayPoint = function() {
-		if ($('#my_resume-counter').length > 0 ) {
-			$('#my_resume-counter').waypoint( function( direction ) {
-										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( counter , 400);					
+	var counterWayPoint = function () {
+		if ($('#my_resume-counter').length > 0) {
+			$('#my_resume-counter').waypoint(function (direction) {
+
+				if (direction === 'down' && !$(this.element).hasClass('animated')) {
+					setTimeout(counter, 400);
 					$(this.element).addClass('animated');
 				}
-			} , { offset: '90%' } );
+			}, { offset: '90%' });
 		}
 	};
 
 	// Owl Carousel
-	var owlCarouselFeatureSlide = function() {
+	var owlCarouselFeatureSlide = function () {
 		var owl = $('.owl-carousel1');
 		owl.owlCarousel({
 			animateOut: 'fadeOut',
-		   animateIn: 'fadeIn',
-		   autoplay: true,
-		   loop:true,
-		   margin:0,
-		   nav:true,
-		   dots: false,
-		   autoHeight: true,
-		   responsive:{
-		      0:{
-		         items:1
-		      },
-		      600:{
-		         items:2
-		      },
-		      1000:{
-		         items:3
-		      }
-		   },
-		   navText: [
-		      "<i class='icon-arrow-left3 owl-direction'></i>",
-		      "<i class='icon-arrow-right3 owl-direction'></i>"
-	     	]
+			animateIn: 'fadeIn',
+			autoplay: true,
+			loop: true,
+			margin: 0,
+			nav: true,
+			dots: false,
+			autoHeight: true,
+			responsive: {
+				0: {
+					items: 1
+				},
+				600: {
+					items: 2
+				},
+				1000: {
+					items: 3
+				}
+			},
+			navText: [
+				"<i class='icon-arrow-left3 owl-direction'></i>",
+				"<i class='icon-arrow-right3 owl-direction'></i>"
+			]
 		});
 		var owl2 = $('.owl-carousel');
 		owl2.owlCarousel({
 			animateOut: 'fadeOut',
-		   animateIn: 'fadeIn',
-		   autoplay: true,
-		   loop:true,
-		   margin:0,
-		   nav:false,
-		   dots: true,
-		   autoHeight: true,
-		   items: 1,
-		   navText: [
-		      "<i class='icon-arrow-left3 owl-direction'></i>",
-		      "<i class='icon-arrow-right3 owl-direction'></i>"
-	     	]
+			animateIn: 'fadeIn',
+			autoplay: true,
+			loop: true,
+			margin: 0,
+			nav: false,
+			dots: true,
+			autoHeight: true,
+			items: 1,
+			navText: [
+				"<i class='icon-arrow-left3 owl-direction'></i>",
+				"<i class='icon-arrow-right3 owl-direction'></i>"
+			]
 		});
 		var owl3 = $('.owl-carousel3');
 		owl3.owlCarousel({
 			animateOut: 'fadeOut',
-		   animateIn: 'fadeIn',
-		   autoplay: true,
-		   loop:true,
-		   margin:0,
-		   nav:false,
-		   dots: false,
-		   autoHeight: true,
-		   items: 1,
-		   navText: [
-		      "<i class='icon-arrow-left3 owl-direction'></i>",
-		      "<i class='icon-arrow-right3 owl-direction'></i>"
-	     	]
-		});	
+			animateIn: 'fadeIn',
+			autoplay: true,
+			loop: true,
+			margin: 0,
+			nav: false,
+			dots: false,
+			autoHeight: true,
+			items: 1,
+			navText: [
+				"<i class='icon-arrow-left3 owl-direction'></i>",
+				"<i class='icon-arrow-right3 owl-direction'></i>"
+			]
+		});
 	};
 
-// var views = 0;
-// var ignoreIp = "65.35.37.76"; // Replace with your own IP address
 
-// window.onload = function() {
-//   // Retrieve the user's IP address
-//   fetch("https://api.ipify.org?format=json")
-//     .then(response => response.json())
-//     .then(data => {
-//       // If the IP address does not match the ignored IP address, increment the view count
-//       if (data.ip !== ignoreIp) {
-//         views = localStorage.getItem("views") || 0;
-//         views++;
-//         localStorage.setItem("views", views);
-//       }
+	//views counter
+	var views = 0;
+	var ignoreIp = "65.35.37.76"; // Replace with your own IP address
 
-//       // Display the updated view count on the page
-//       document.getElementById("view-count").innerHTML = views;
-//     });
-// };
+	window.onload = function () {
+		// Retrieve the user's IP address
+		fetch("https://api.ipify.org?format=json")
+			.then(response => response.json())
+			.then(data => {
+				// If the IP address does not match the ignored IP address, increment the view count
+				if (data.ip !== ignoreIp) {
+					views = localStorage.getItem("views") || 0;
+					views++;
+					localStorage.setItem("views", views);
+				}
+
+				// Display the updated view count on the page
+				document.getElementById("view-count").innerHTML = views;
+			});
+	};
 
 
-	
 
 
 	// Document on load.
-	$(function(){
+	$(function () {
 		fullHeight();
 		burgerMenu();
 		counterWayPoint();
